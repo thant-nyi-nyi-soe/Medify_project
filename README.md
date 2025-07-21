869647ecb8deb250c58f438897f0645f7ad713b9
thant-nyi-nyi-soe <thantnyinyisoe.qualymm@gmail.com>
2025-07-21 16:43:39 +0630

    more interactions added to header

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Medify_project 
+ font-family: "Nunito", sans-serif;
+ font-family: "Ubuntu", sans-serif;
--- assets/js/common.js
+++ assets/js/common.js
@@ -4,55 +4,99 @@ document.addEventListener("DOMContentLoaded", () => {
   .then(res => res.text())
   .then(html => {
     document.querySelector('header').innerHTML = html;
+    
     const hamburger = document.querySelector(".hamburger-menu");
     const sidebar = document.querySelector(".sidebar");
+    const header = document.querySelector(".header-container"); 
+    const overlay = document.querySelector(".overlay");
 
     hamburger.addEventListener("click", () => {
       hamburger.classList.toggle("active");
-      sidebar.classList.toggle("show");
+
+      hamburgerToggle();
     });
 
-    const back = document.querySelector(".back-button");
-
+    const back = document.querySelector(".close-button");
     back.addEventListener("click", () => {
       hamburger.classList.toggle("active");  
-      sidebar.classList.toggle("show");
+
+      hamburgerToggle();
     });
+
+    function hamburgerToggle() {
+      if (hamburger.classList.contains("active")) {
+      sidebar.classList.add("show");
+      header.classList.add("header-hidden");
+      overlay.classList.add("dimmed");
+    } 
+    else {
+      sidebar.classList.remove("show");
+      header.classList.remove("header-hidden");
+      overlay.classList.remove("dimmed");
+    }
+    }
 
     const home = document.querySelector(".sidebar-home");
     const blog = document.querySelector(".sidebar-blog");
     const contacts = document.querySelector(".sidebar-contacts");
 
-    const homeHr = document.querySelector(".home-hr");
+    const homeHr = document.querySelector(".home-option");
     const blogHr = document.querySelector(".blog-hr");
     const contactsHr = document.querySelector(".contacts-hr");
 
-    home.addEventListener("click",() => {
-      home.classList.add("color-active");
-      blog.classList.remove("color-active");
-      contacts.classList.remove("color-active");
-      homeHr.classList.add("line");
-      blogHr.classList.remove("line");
-      contactsHr.classList.remove("line");
+    // home.addEventListener("click",() => {
+    //   home.classList.add("color-active");
+    //   blog.classList.remove("color-active");
+    //   contacts.classList.remove("color-active");
+    //   homeHr.classList.add("line");
+    //   blogHr.classList.remove("line");
+    //   contactsHr.classList.remove("line");
+    // });
+
+    // blog.addEventListener("click", () => {
+    //   blog.classList.add("color-active");
+    //   home.classList.remove("color-active");
+    //   contacts.classList.remove("color-active");
+    //   homeHr.classList.remove("line");
+    //   blogHr.classList.add("line");
+    //   contactsHr.classList.remove("line");
+    // });
+
+    //  contacts.addEventListener("click", () => {
+    //   contacts.classList.add("color-active");
+    //   home.classList.remove("color-active");
+    //   blog.classList.remove("color-active");
+    //   homeHr.classList.remove("line");
+    //   blogHr.classList.remove("line");
+    //   contactsHr.classList.add("line");
+    // });
+
+    const arrow = document.querySelector(".pages-arrow");
+    const pagesList = document.querySelector(".pages-list");
+
+    arrow.addEventListener("click", () => {
+      arrow.classList.toggle("arrow-rotate");
+
+      arrowToggle();
     });
 
-    blog.addEventListener("click", () => {
-      blog.classList.add("color-active");
-      home.classList.remove("color-active");
-      contacts.classList.remove("color-active");
-      homeHr.classList.remove("line");
-      blogHr.classList.add("line");
-      contactsHr.classList.remove("line");
+      
+    const sidebarPages = document.querySelector(".sidebar-pages");
+
+    sidebarPages.addEventListener("click", () => {
+      arrow.classList.toggle("arrow-rotate");
+
+      arrowToggle();
     });
 
-     contacts.addEventListener("click", () => {
-      contacts.classList.add("color-active");
-      home.classList.remove("color-active");
-      blog.classList.remove("color-active");
-      homeHr.classList.remove("line");
-      blogHr.classList.remove("line");
-      contactsHr.classList.add("line");
-    });
+     function arrowToggle() {
+      if (arrow.classList.contains("arrow-rotate")) {
+        pagesList.classList.add("show-list");
+      }
+      else {
+        pagesList.classList.remove("show-list");
+      }
+    }
 
   });
   
